b0783c022c4677be2b988003f0ea8bdd254643af
nonsof <113558928+nonsof@users.noreply.github.com>
2023-10-29 21:03:35 +0300

    # Функция для добавления заметки

--- notes.py
+++ notes.py
@@ -4,3 +4,15 @@ from datetime import datetime
 
 # Путь к файлу для хранения заметок
 data_file = 'notes.json'
+# Функция для добавления заметки
+def add_note(title, message):
+    # Создаем заметку
+    note = {
+        'id': len(notes) + 1,
+        'title': title,
+        'message': message,
+        'timestamp': datetime.now().isoformat()
+    }
+    # Добавляем заметку в список
+    notes.append(note)
+    print('Заметка успешно добавлена.')
